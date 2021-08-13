2a5c5fb80332acddf3e140b9afb67645f759ff3f
Hasitha-Su <Hasithakms@gmail.com>
2021-08-13 18:05:55 +0530

    update registration form and routing for registration form

--- EPass-App/src/app/app-routing.module.ts
+++ EPass-App/src/app/app-routing.module.ts
@@ -2,14 +2,18 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ViewEpassComponent } from './epass/view-epass/view-epass.component';
 import { RequestEpassComponent } from './epass/request-epass/request-epass.component';
+import { RegistrationComponent } from './citizen/registration/registration.component';
 
 const routes: Routes = [
   { path: 'epass/viewAll', component: ViewEpassComponent },
   { path: 'epass/request', component: RequestEpassComponent },
+
+  { path: 'citizen/signup', component: RegistrationComponent },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
+
 export class AppRoutingModule { }
