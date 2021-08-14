0180a317f07d51cc289225c559d5995f9547bd1b
Hasitha-Su <Hasithakms@gmail.com>
2021-08-14 22:49:20 +0530

    update nav bar

--- EPass-App/src/app/shared/shared.module.ts
+++ EPass-App/src/app/shared/shared.module.ts
@@ -3,17 +3,18 @@ import { CommonModule } from '@angular/common';
 import { FooterComponent } from './footer/footer.component';
 import { HeaderComponent } from './header/header.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
-
-
+import { AppRoutingModule } from '../app-routing.module';
 
 @NgModule({
   declarations: [
     FooterComponent,
     HeaderComponent,
     PageNotFoundComponent
+    
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    AppRoutingModule
   ],
   exports: [
     FooterComponent,
